eeeef7bf76f6bb16ee5c649d72749044e14cae99
浅田清文 <kiyofumi.a.0122@gmail.com>
2022-06-25 00:14:35 +0900

    条件分岐を修正

--- client/src/presentation/components/block/table-body.tsx
+++ client/src/presentation/components/block/table-body.tsx
@@ -9,94 +9,146 @@ type TProps = {
   data: TTask;
 };
 const TableBody: React.FC<TProps> = ({ data }) => {
-  //TODO: rowSpanを入れた際のmap処理を修正
-  const [rowSpan, setRowSpan] = useState<number>(100);
   const [defaultSelectId, setDefaultSelectId] = useState<number>(-1);
-  const dispatch = useDispatch;
-  const convertProgressInt2percent = (v: number): number => v * 100;
-  const selectedOption = (optionId: any) => {
-    //TODO: ---の初期値に戻せる必要がある
-    const setId = optionId;
+  const workContentsRef = React.useRef<HTMLInputElement>(null);
+  const manDayRef = React.useRef<HTMLInputElement>(null);
+  const requesterRef = React.useRef<HTMLInputElement>(null);
+  const progressRef = React.useRef<HTMLInputElement>(null);
+  const noteRef = React.useRef<HTMLInputElement>(null);
+
+  const convertProgress = (progress: number): number => progress * 100;
+  const selectedOption = (e: any) => {
+    const setId = e;
     setDefaultSelectId(setId);
   };
+  const handleSave = (dataId: any) => {
+    console.log('v.dataId', dataId);
+    console.log('workContentsRef', workContentsRef.current?.value);
+    console.log('manDayRef', manDayRef.current?.value);
+    console.log('requesterRef', requesterRef.current?.value);
+    console.log('progressRef', progressRef.current?.value);
+    console.log('noteRef', noteRef.current?.value);
+  };
   const handleDelete = () => {
-    //TODO: delete機能実装
     console.log();
   };
+  const isAchieve = (int: number): boolean => 100 <= convertProgress(int);
 
+  console.log(workContentsRef.current);
   return (
     <tbody>
       {data.progressData?.map((v) => (
         <tr key={v.dataId}>
-          {/*deleteがtrueの場合の表示・非表示*/}
           {v.delete ? null : (
             <>
-              {/* progressが100%の場合グレーアウト add bg-gray-300 */}
-              {convertProgressInt2percent(v.progress) === 100 ? (
-                <>
-                  <td rowSpan={1} className="flex-none border py-1 text-center text-xs">
-                    {data.userName}
-                  </td>
-                  <td className="flex-none border bg-gray-300 py-1 text-xs">
-                    <label>
-                      <select defaultValue={v.selectedOptionId} onChange={(e): void => selectedOption(e.target.value)}>
-                        <option value={defaultSelectId}>---</option>
-                        {v.options.map((option) => (
-                          <option defaultValue={v.selectedOptionId} key={option.id} value={option.id}>
-                            {option.label}
-                          </option>
-                        ))}
-                      </select>
-                    </label>
-                  </td>
-                  <td className="flex-none border bg-gray-300 px-1 py-1 text-xs">{v.workContents}</td>
-                  <td className="flex-none border bg-gray-300 py-1 text-center text-xs">{v.manDay}</td>
-                  <td className="flex-none border bg-gray-300 py-1 text-center text-xs">{v.requester}</td>
-                  <td className="flex-none border bg-gray-300 py-1 text-center text-xs">
-                    {convertProgressInt2percent(v.progress)}%
-                  </td>
-                  <td className="flex-none border bg-gray-300 px-1 py-1 text-xs">{v.note}</td>
-                  <td className="flex-none border bg-gray-300 py-1 text-center text-xs">
-                    <div
-                      className="cursor-pointer bg-gray-300 px-2 py-1 hover:text-red-600"
-                      onClick={() => handleDelete()}
-                    >
-                      <FontAwesomeIcon icon={faTrashCan} />
-                    </div>
-                  </td>
-                </>
-              ) : (
-                <>
-                  <td rowSpan={1} className="flex-none border py-1 text-center text-xs">
-                    {data.userName}
-                  </td>
-                  <td className="flex-none border py-1 text-xs">
-                    <label>
-                      <select defaultValue={v.selectedOptionId} onChange={(e): void => selectedOption(e.target.value)}>
-                        <option value={defaultSelectId}>---</option>
-                        {v.options.map((option) => (
-                          <option defaultValue={v.selectedOptionId} key={option.id} value={option.id}>
-                            {option.label}
-                          </option>
-                        ))}
-                      </select>
-                    </label>
-                  </td>
-                  <td className="flex-none border px-1 py-1 text-xs">{v.workContents}</td>
-                  <td className="flex-none border py-1 text-center text-xs">{v.manDay}</td>
-                  <td className="flex-none border py-1 text-center text-xs">{v.requester}</td>
-                  <td className="flex-none border py-1 text-center text-xs">
-                    {convertProgressInt2percent(v.progress)}%
-                  </td>
-                  <td className="flex-none border px-1 py-1 text-xs">{v.note}</td>
-                  <td className="flex-none border py-1 text-center text-xs">
-                    <div className="cursor-pointer px-2 py-1 hover:text-red-600" onClick={() => handleDelete()}>
-                      <FontAwesomeIcon icon={faTrashCan} />
-                    </div>
-                  </td>
-                </>
-              )}
-              <td rowSpan={1} className="flex-none border px-4 py-1 text-xs"></td>
+              {/* 担当者 */}
+              <td rowSpan={1} className="flex-none border py-1 text-center text-xs">
+                {data.userName}
+              </td>
+              {/* 案件名 */}
+              <td
+                className={
+                  isAchieve(v.progress) ? 'flex-none border bg-gray-300 py-1 text-xs' : 'flex-none border py-1 text-xs'
+                }
+              >
+                <select
+                  className={isAchieve(v.progress) ? ' bg-gray-300' : ''}
+                  defaultValue={v.selectedOptionId}
+                  onChange={(e): void => selectedOption(e.target.value)}
+                >
+                  <option value={defaultSelectId}>---</option>
+                  {v.options.map((option) => (
+                    <option defaultValue={v.selectedOptionId} key={option.id} value={option.id}>
+                      {option.label}
+                    </option>
+                  ))}
+                </select>
+              </td>
+              {/* 作業内容 */}
+              <td
+                className={
+                  isAchieve(v.progress)
+                    ? 'flex-none border bg-gray-300 px-1 py-1 text-xs'
+                    : 'flex-none border px-1 py-1 text-xs'
+                }
+              >
+                {v.workContents}
+              </td>
+              {/* 人日 */}
+              <td
+                className={
+                  isAchieve(v.progress)
+                    ? 'flex-none border bg-gray-300 py-1 text-center text-xs'
+                    : 'flex-none border py-1 text-center text-xs'
+                }
+              >
+                {v.manDay}
+              </td>
+              {/* 依頼者 */}
+              <td
+                className={
+                  isAchieve(v.progress)
+                    ? 'flex-none border bg-gray-300 py-1 text-center text-xs'
+                    : 'flex-none border py-1 text-center text-xs'
+                }
+              >
+                {v.requester}
+              </td>
+              {/* 進捗 */}
+              <td
+                className={
+                  isAchieve(v.progress)
+                    ? 'flex-none border bg-gray-300 py-1 text-center text-xs'
+                    : 'flex-none border py-1 text-center text-xs'
+                }
+              >
+                <div className="flex">
+                  <input
+                    className={isAchieve(v.progress) ? 'flex-1 bg-gray-300 text-right' : 'flex-1 text-right'}
+                    type="text"
+                    name="progress"
+                    placeholder="0-100"
+                    defaultValue={convertProgress(v.progress)}
+                    ref={progressRef}
+                  />
+                  <div className="flex-1">%</div>
+                </div>
+              </td>
+              {/* 備考 */}
+              <td
+                className={
+                  isAchieve(v.progress)
+                    ? 'flex-none border bg-gray-300 px-1 py-1 text-xs'
+                    : 'flex-none border px-1 py-1 text-xs'
+                }
+              >
+                {v.note}
+              </td>
+              {/* 削除 */}
+              <td
+                className={
+                  isAchieve(v.progress)
+                    ? 'flex-none border bg-gray-300 py-1 text-center text-xs'
+                    : 'flex-none border py-1 text-center text-xs'
+                }
+              >
+                <div
+                  className={
+                    isAchieve(v.progress)
+                      ? 'cursor-pointer bg-gray-300 px-2 py-1 hover:text-red-600'
+                      : 'cursor-pointer px-2 py-1 hover:text-red-600'
+                  }
+                  onClick={() => handleDelete()}
+                >
+                  <FontAwesomeIcon icon={faTrashCan} />
+                </div>
+              </td>
+              {/* 保存 */}
+              <td rowSpan={1} className="flex-none border px-4 py-1 text-xs">
+                <div className="cursor-pointer" onClick={handleSave}>
+                  保
+                </div>
+              </td>
             </>
           )}
         </tr>
--- client/src/presentation/components/block/table-header.tsx
+++ client/src/presentation/components/block/table-header.tsx
@@ -1,12 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFileCsv } from '@fortawesome/free-solid-svg-icons';
+import { faFileArrowDown } from '@fortawesome/free-solid-svg-icons';
 import { format } from 'date-fns';
 import { CSVLink } from 'react-csv';
 import { THeaderData, TTaskArray } from '../../../types/table';
 import { fromPairs } from 'ramda';
-
 type TProps = {
   headerData: THeaderData;
   dataList: TTaskArray;
@@ -18,7 +17,7 @@ const TableHeader: React.FC<TProps> = ({ headerData, dataList }) => {
   const today = format(new Date(), 'yyyyMMdd');
 
   return (
-    <thead className="table-fixed bg-blue-400">
+    <thead className="table-fixed bg-slate-900 text-white">
       <tr>
         <th className="w-1/12 border-2 border-solid py-2 text-xs">{header.user}</th>
         <th className="w-1/12 border-2 border-solid py-2 text-xs">{header.projectName}</th>
@@ -28,11 +27,11 @@ const TableHeader: React.FC<TProps> = ({ headerData, dataList }) => {
         <th className="w-1/12 border-2 border-solid py-2 text-xs">{header.progress}</th>
         <th className="w-3/12 border-2 border-solid py-2 text-xs">{header.note}</th>
         <th className="border-2 border-solid py-2 text-xs">{header.delete}</th>
-        <th className="border-2 border-solid py-2 text-xs">
+        <th className="w-1/12 border-2 border-solid py-2 text-xs">
           <div className="cursor-pointer text-sm hover:text-blue-700">
             {/* TODO:CSV機能を使えるよう修正する */}
             <CSVLink filename={`task_progress_${today}.csv`} data={dataList} headers={headerData}>
-              <FontAwesomeIcon icon={faFileCsv} />
+              <FontAwesomeIcon icon={faFileArrowDown} />
             </CSVLink>
           </div>
         </th>
